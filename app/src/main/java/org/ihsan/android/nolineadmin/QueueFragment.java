253fb7391409458c90bd3baf27140e2d824635ae
Ihsan <ihsan.lee@gmail.com>
2015-05-12 10:22:45 +0800

    User ListView to show the subqueue

--- app/src/main/java/org/ihsan/android/nolineadmin/QueueFragment.java
+++ app/src/main/java/org/ihsan/android/nolineadmin/QueueFragment.java
@@ -12,7 +12,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,14 +26,18 @@ import java.util.ArrayList;
 public class QueueFragment extends Fragment {
     private static final String TAG = "QueueFragment";
 
-    private LinearLayout mSubqueueLinearLayout;
+    private ListView mSubqueueListView;
+
+    private ArrayList<Subqueue> mSubqueues = new ArrayList<Subqueue>();
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_queue, container, false);
-        mSubqueueLinearLayout = (LinearLayout) view.findViewById(R.id.subqueue_linearLayout);
+        mSubqueueListView = (ListView) view.findViewById(R.id.subqueue_list_view);
+        mSubqueueListView.setAdapter(new SubqueueAdapter(mSubqueues));
         new GetDetailTask().execute();
         return view;
     }
@@ -73,47 +79,17 @@ public class QueueFragment extends Fragment {
         @Override
         protected void onPostExecute(final ArrayList<Subqueue> subqueues) {
             if (subqueues != null) {
-                mSubqueueLinearLayout.removeAllViews();
-                for (Subqueue subqueue : subqueues) {
-                    final int index = subqueues.indexOf(subqueue);
-                    final String name = subqueue.getName();
-                    View subqueueView = getActivity().getLayoutInflater().inflate(R.layout.subqueue_item, mSubqueueLinearLayout, false);
-                    TextView subqueueNameTextView = (TextView) subqueueView.findViewById(R.id.subqueue_name_textView);
-                    LinearLayout descriptionView = (LinearLayout) subqueueView.findViewById(R.id.description_view);
-                    TextView subqueueTotalTextView = (TextView) subqueueView.findViewById(R.id.subqueue_total_textView);
-                    TextView subqueueFirstNumberTextView = (TextView) subqueueView.findViewById(R.id.subqueue_first_number_textView);
-                    TextView subqueueLeftButton = (TextView) subqueueView.findViewById(R.id.subquque_left_button);
-                    TextView subqueueRightButton = (TextView) subqueueView.findViewById(R.id.subquque_right_button);
-                    subqueueNameTextView.setText(name);
-                    subqueueTotalTextView.setText(String.valueOf(subqueue.getTotal()));
-                    descriptionView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent(getActivity(),UserListActivity.class);
-                            intent.putExtra(UserListFragment.EXTRA_SUBQUEUE_NUMBER, index);
-                            intent.putExtra(UserListFragment.EXTRA_SUBQUEUE_NAME, name);
-                            startActivity(intent);
-                        }
-                    });
-                    subqueueFirstNumberTextView.setText(String.valueOf(subqueue.getFirstNumber()));
-                    subqueueLeftButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            new NextQueuerTask().execute(index, 2);
-                        }
-                    });
-                    subqueueRightButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            new NextQueuerTask().execute(index, 1);
-                        }
-                    });
-                    mSubqueueLinearLayout.addView(subqueueView, index);
-                }
+                mSubqueues.clear();
+                mSubqueues.addAll(subqueues);
+                updateAdapter();
             } else {
                 Toast.makeText(getActivity(), "获取队列信息失败，请重试", Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    public void updateAdapter() {
+        ((SubqueueAdapter) mSubqueueListView.getAdapter()).notifyDataSetChanged();
     }
 
     private class NextQueuerTask extends AsyncTask<Integer, Void, Boolean> {
@@ -121,7 +97,8 @@ public class QueueFragment extends Fragment {
         protected Boolean doInBackground(Integer... params) {
             int queueId = PreferenceManager.getDefaultSharedPreferences(getActivity())
                     .getInt(getString(R.string.logined_queue_id), -1);
-            return new DataFetcher(getActivity()).fetchNextQueuerResult(queueId, params[0], params[1]);
+            return new DataFetcher(getActivity()).fetchNextQueuerResult(queueId, params[0],
+                    params[1]);
         }
 
         @Override
@@ -131,6 +108,62 @@ public class QueueFragment extends Fragment {
             } else {
                 Toast.makeText(getActivity(), "处理失败，请重试", Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+    private class SubqueueAdapter extends ArrayAdapter<org.ihsan.android.nolineadmin.Subqueue> {
+        public SubqueueAdapter(ArrayList<Subqueue> subqueues) {
+            super(getActivity(), 0, subqueues);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.subqueue_item, null);
+            }
+
+            Subqueue subqueue = getItem(position);
+            final int index = position;
+            final String name = subqueue.getName();
+            TextView subqueueNameTextView = (TextView) convertView.findViewById(R.id
+                    .subqueue_name_textView);
+            LinearLayout descriptionView = (LinearLayout) convertView.findViewById(R.id
+                    .description_view);
+            TextView subqueueTotalTextView = (TextView) convertView.findViewById(R.id
+                    .subqueue_total_textView);
+            TextView subqueueFirstNumberTextView = (TextView) convertView.findViewById(R
+                    .id.subqueue_first_number_textView);
+            TextView subqueueLeftButton = (TextView) convertView.findViewById(R.id
+                    .subquque_left_button);
+            TextView subqueueRightButton = (TextView) convertView.findViewById(R.id
+                    .subquque_right_button);
+            subqueueNameTextView.setText(name);
+            subqueueTotalTextView.setText(String.valueOf(subqueue.getTotal()));
+            descriptionView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), UserListActivity.class);
+                    intent.putExtra(UserListFragment.EXTRA_SUBQUEUE_NUMBER, index);
+                    intent.putExtra(UserListFragment.EXTRA_SUBQUEUE_NAME, name);
+                    startActivity(intent);
+                }
+            });
+            subqueueFirstNumberTextView.setText(String.valueOf(subqueue.getFirstNumber()));
+            subqueueLeftButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new NextQueuerTask().execute(index, 2);
+                }
+            });
+            subqueueRightButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new NextQueuerTask().execute(index, 1);
+                }
+            });
+
+            return convertView;
         }
     }
 }
